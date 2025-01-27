9e66cbaedbce22e72db3ad99c9516ab25e17df68
nhs0912 <nhs0912@nate.com>
2025-01-27 19:51:22 +0900

    feat: first nuxt and quasar framework

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+    modules: [
+    'nuxt-quasar-ui'
+  ],
   devtools: { enabled: true },
   typescript : {
-    shim: false
-  }
+    shim: false,
+    typeCheck: true,
+  },  
+  ssr: false
+  // mode:'spa',
+  // build: {
+  //   ssr: false,
+  // }
 })
