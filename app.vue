fdaae754255bcf19999f88c1d6de18d8ca862c06
nhs0912 <nhs0912@nate.com>
2025-01-29 23:32:44 +0900

    feat: nuxtapp(Runtime App Context)

--- app.vue
+++ app.vue
@@ -5,5 +5,7 @@
 </NuxtLayout>
 </template>
 <script setup lang="ts">
-
+const nuxtApp = useNuxtApp();
+console.log(nuxtApp);
+// nuxtApp.provide('hello', (msg : string) => `hello ${msg}`);
 </script>
