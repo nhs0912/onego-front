d019944ef42746748c059cba36d3102b7ee1df83
nhs0912 <nhs0912@nate.com>
2025-01-30 17:16:54 +0900

    feat: Route Middleware 기초

--- pages/course/index.vue
+++ pages/course/index.vue
@@ -7,9 +7,7 @@
           웹개발 입문부터, Vue 프론트엔드 실전 그리고 실무 활용법까지 함께
           시작해봐요!
         </div>
-        <q-btn label="시작하기" 
-        class="q-mt-lg" size="lg" color="primary" 
-        unelevated to="/course/html-css" />
+        <q-btn label="시작하기" class="q-mt-lg" size="lg" color="primary" unelevated to="/course/html-css" />
       </div>
     </AppCard>
   </div>
