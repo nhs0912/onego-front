21cb66c9a4f25e40932207b66a967ef7d74df92d
david Noh <nhs0912@nate.com>
2025-02-20 02:16:03 +0900

    feat: modules: @Nuxt/i18n 설치

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   modules: [
-    'nuxt-quasar-ui',
-    '@pinia/nuxt', // needed
+    'nuxt-quasar-ui', // needed
+    '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/i18n',
   ],
+  i18n: {
+    locales: ['en', 'ko'], // URL 경로 접두사에 사용됨
+    defaultLocale: 'ko', // Nuxt 페이지 및 라우팅을 위한 프로젝트의 기본 로케일
+    vueI18n: './i18n.config.ts',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root', // recommended
+    },
+  },
   devtools: { enabled: true },
   typescript: {
     shim: false,
@@ -13,12 +24,12 @@ export default defineNuxtConfig({
   },
   ssr: true,
   imports: {
-    presets: [
-      {
-        from: 'vue-i18n',
-        imports: ['useI18n'],
-      },
-    ],
+    // presets: [
+    //   {
+    //     from: 'vue-i18n',
+    //     imports: ['useI18n'],
+    //   },
+    // ],
   },
   quasar: {
     plugins: ['Notify'],
--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -1,37 +1,38 @@
-import { createI18n } from 'vue-i18n';
-export default defineNuxtPlugin(({ vueApp }) => {
-  const i18n = createI18n({
-    legacy: false,
-    globalInjection: true,
-    locale: useCookie('locale').value || useDefaultLocale().value,
-    messages: {
-      en: {
-        home: 'Home',
-        about: 'About',
-        youtube: 'Youtube',
-        admin: 'Admin',
-        custom: 'Custom',
-        course: 'Course',
-        login: 'Login',
-        logout: 'Logout',
-        hello: 'Hello',
-        email: 'email',
-        password: 'password',
-      },
-      ko: {
-        home: '홈',
-        about: '어바웃',
-        youtube: '유튜브',
-        admin: '관리자',
-        custom: '커스텀',
-        course: '코스',
-        login: '로그인',
-        logout: '로그아웃',
-        hello: '안녕하세요',
-        email: '이메일',
-        password: '비밀번호',
-      },
-    },
-  });
-  vueApp.use(i18n);
+// import { createI18n } from 'vue-i18n';
+export default defineNuxtPlugin(() => {
+  // export default defineNuxtPlugin(({ vueApp }) => {
+  // const i18n = createI18n({
+  // legacy: false,
+  // globalInjection: true,
+  // locale: useCookie('locale').value || useDefaultLocale().value,
+  // messages: {
+  //   en: {
+  //     home: 'Home',
+  //     about: 'About',
+  //     youtube: 'Youtube',
+  //     admin: 'Admin',
+  //     custom: 'Custom',
+  //     course: 'Course',
+  //     login: 'Login',
+  //     logout: 'Logout',
+  //     hello: 'Hello',
+  //     email: 'email',
+  //     password: 'password',
+  //   },
+  //   ko: {
+  //     home: '홈',
+  //     about: '어바웃',
+  //     youtube: '유튜브',
+  //     admin: '관리자',
+  //     custom: '커스텀',
+  //     course: '코스',
+  //     login: '로그인',
+  //     logout: '로그아웃',
+  //     hello: '안녕하세요',
+  //     email: '이메일',
+  //     password: '비밀번호',
+  //   },
+  // },
+  // });
+  // vueApp.use(i18n);
 });
