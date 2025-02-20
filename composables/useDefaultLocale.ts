c115207eac45d841570541a7113846f0d3f76380
nhs0912 <nhs0912@nate.com>
2025-02-21 08:12:43 +0900

    feat: Data Fetching caching bug

--- composables/useDefaultLocale.ts
+++ composables/useDefaultLocale.ts
@@ -1,15 +1,15 @@
 export const useDefaultLocale = (fallback = 'en') => {
-    const locale = ref(fallback);
-    if (process.server) {
-        const reqLocale = useRequestHeaders()['accept-language']?.split(',')[0];
-        if (reqLocale) {
-            locale.value = reqLocale;
-        }
-    } else if (process.client) {
-        const navLang = navigator.language;
-        if (navLang) {
-            locale.value = navLang;
-        }
+  const locale = ref(fallback);
+  if (process.server) {
+    const reqLocale = useRequestHeaders()['accept-language']?.split(',')[0];
+    if (reqLocale) {
+      locale.value = reqLocale;
     }
-    return locale;
+  } else if (process.client) {
+    const navLang = navigator.language;
+    if (navLang) {
+      locale.value = navLang;
+    }
+  }
+  return locale;
 };
